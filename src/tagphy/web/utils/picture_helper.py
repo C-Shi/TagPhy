9ce98f611e1609b380b9dd78c9a7f77251a991f9
Cheng <cshi@ualberta.ca>
2026-08-09 17:30:01 -0600

    Update Picture Helper to be able to handle basic pagination

--- src/tagphy/web/utils/picture_helper.py
+++ src/tagphy/web/utils/picture_helper.py
@@ -1,25 +1,15 @@
 from typing import Any
 
+# set default picture per page
+_PAGINATION_DEFAULT_ = 25
+
 
 class PictureHelper:
-    def __init__(self, db: Any):
+    def __init__(self, db: Any, page_size: int = _PAGINATION_DEFAULT_):
+        self.page_size = page_size
         self.db = db
 
-    def get_pictures_for_tag(self, tag_id: int):
-        """Get all pictures for a tag."""
-        try:
-
-            images_query = f"""
-                SELECT i.*
-                FROM images i
-                JOIN image_tags it ON i.id = it.image_id
-                WHERE it.tag_id = ?)
-            """
-            return self.db.query(images_query, (str(tag_id),))
-        except ValueError as e:
-            raise ValueError(f"Invalid Tag")
-
-    def get_pictures_for_tags_flatted(self, tag_ids: list[int]):
+    def get_pictures_for_tags_flatted(self, tag_ids: list[int], page: int = 1):
         """Get all pictures for a list of tags flattened into a single list."""
 
         try:
@@ -28,6 +18,8 @@ class PictureHelper:
                 FROM images i
                 JOIN image_tags it ON i.id = it.image_id
                 WHERE it.tag_id IN ({",".join(["?"] * len(tag_ids))})
+                ORDER BY i.id DESC
+                LIMIT {self.page_size} OFFSET {(page - 1) * self.page_size}
             """
             return self.db.query(images_query, [str(id) for id in tag_ids])
         except ValueError as e:
