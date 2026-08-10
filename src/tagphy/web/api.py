4a59c4e9aa22ab4dc3d5ff0373c9070252d709e9
Cheng <cshi@ualberta.ca>
2026-08-09 23:57:47 -0600

    Add backend routing and helper for retriving pictures

--- src/tagphy/web/api.py
+++ src/tagphy/web/api.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Response
 from tagphy.db import SQLiteConnection
 from tagphy.web.utils.tag_helper import TagHelper
 from tagphy.web.utils.picture_helper import PictureHelper
@@ -24,5 +24,25 @@ async def get_tag(tag_id: int):
         images = picture_helper.get_pictures_for_tags_flatted(image_tag_ids)
         tag_pictures_response["pictures"] = [dict(image) for image in images]
         return tag_pictures_response
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+
+@router.get("/pictures")
+async def get_pictures():
+    try:
+        pictures = picture_helper.get_pictures_for_tags_flatted()
+        return pictures
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+
+@router.get("/pictures/{picture_id}/preview")
+async def get_picture_preview(picture_id: int):
+    try:
+        preview = picture_helper.get_picture_preview(picture_id)
+        return Response(
+            content=preview[0], media_type="image/jpeg", headers={"Alt": preview[1]}
+        )
     except ValueError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
--- src/tagphy/web/utils/picture_helper.py
+++ src/tagphy/web/utils/picture_helper.py
@@ -1,4 +1,11 @@
+import io
+import os
 from typing import Any
+
+from PIL import Image
+
+from tagphy import app_root
+
 
 # set default picture per page
 _PAGINATION_DEFAULT_ = 25
@@ -9,18 +16,47 @@ class PictureHelper:
         self.page_size = page_size
         self.db = db
 
-    def get_pictures_for_tags_flatted(self, tag_ids: list[int], page: int = 1):
+    def get_pictures_for_tags_flatted(self, tag_ids: list[int] = [], page: int = 1):
         """Get all pictures for a list of tags flattened into a single list."""
 
         try:
             images_query = f"""
                 SELECT i.*
                 FROM images i
-                JOIN image_tags it ON i.id = it.image_id
-                WHERE it.tag_id IN ({",".join(["?"] * len(tag_ids))})
+            """
+
+            if len(tag_ids) > 0:
+                images_query += f"""
+                    JOIN image_tags it ON i.id = it.image_id
+                    WHERE it.tag_id IN ({",".join(["?"] * len(tag_ids))})
+                """
+
+            images_query += f"""
                 ORDER BY i.id DESC
                 LIMIT {self.page_size} OFFSET {(page - 1) * self.page_size}
             """
+
             return self.db.query(images_query, [str(id) for id in tag_ids])
         except ValueError as e:
             raise ValueError(f"Invalid Tags")
+
+    def get_picture_preview(self, picture_id: int) -> tuple[bytes, str]:
+
+        try:
+            picture = self.db.select(
+                "images", ["file_name", "file_path"], {"id": picture_id}
+            )[0]
+
+            image_path = os.path.join(app_root(), picture["file_path"])
+
+            with Image.open(image_path) as img:
+                rgb = img.convert("RGB")
+                rgb.thumbnail((320, 320))
+
+                # create a byte buffer to store the image
+                buffer = io.BytesIO()
+                rgb.save(buffer, format="JPEG", quality=85)
+                alt = picture["file_name"]
+            return (buffer.getvalue(), alt)
+        except Exception as e:
+            raise Exception(f"Failed to get picture preview: {e}")
