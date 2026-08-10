ec8cdde31a78e8e76c3c6b790ae1cc58b6b31b18
Cheng <cshi@ualberta.ca>
2026-08-09 23:29:43 -0600

    rename front end /tags to /library to match new design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
-import { AppShell } from './components/AppShell'
-import { LibraryPage } from './pages/LibraryPage'
-import { TagPicturesPage } from './pages/TagPicturesPage'
+import { Navigate, Route, Routes } from "react-router-dom";
+import { AppShell } from "./components/AppShell";
+import { LibraryPage } from "./pages/LibraryPage";
+import { TagPicturesPage } from "./pages/TagPicturesPage";
 
 export default function App() {
   return (
     <Routes>
       <Route element={<AppShell />}>
         <Route index element={<Navigate to="/tags" replace />} />
-        <Route path="/tags" element={<LibraryPage />} />
+        <Route path="/library" element={<LibraryPage />} />
         <Route path="/tags/:tagId/pictures" element={<TagPicturesPage />} />
         <Route path="*" element={<Navigate to="/tags" replace />} />
       </Route>
     </Routes>
-  )
+  );
 }
